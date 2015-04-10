fe6d069544285c1341393cd9830aadf41a878071
Arturo Isai Castro Perpuli <arturo.castrop@live.com>
2015-04-10 04:42:09 +0100

    Add results for no-if version, and script for absolute differences with original version

--- axle/phi/ocl4/results/test.sh
+++ axle/phi/ocl4/results/test.sh
@@ -5,5 +5,8 @@ sizes=(31 63 127 255 511 1023)
 for s in "${sizes[@]}"
 do
     echo $s
-    cmp out$s ../../../../previous_work/ocl4/results/out$s
+    if ! cmp -s out$s ../../../../previous_work/ocl4/results/out$s
+    then
+	./compare.py $s > abs_diff_$s
+    fi
 done
